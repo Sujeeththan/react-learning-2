b532ddcfba4bbdc9fdfef71912262b1421990165
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-21 16:34:21 +0530

    useSet age add

--- src/componensts/hooks/UseState.jsx
+++ src/componensts/hooks/UseState.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 
 function UseState() {
   const [count, setCount] = useState(0);
+  const [age, setAge] = useState(27);
 
   function increase() {
      setCount(count + 1);
@@ -9,6 +10,10 @@ function UseState() {
 
   function decrease() {
     setCount(count - 1);
+  }
+
+  function countIncrease() {
+    setAge(age + 1)
   }
 
 
@@ -19,8 +24,16 @@ function UseState() {
       <button>{count}</button>
       <br />
       <button onClick={decrease}>-</button>
+      <br /> <br />
+      <hr />
+      <button onClick={countIncrease} >Age : {age}</button> <br />
+      <input type="text" value={age} />
     </div>
   )
 }
+
+
+
+
 
 export default UseState
