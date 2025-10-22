cc469196e049861d857443f269292361b78ea290
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-22 16:23:05 +0530

    onChange added  and defaultValue added

--- src/componensts/forms/StudentForm.jsx
+++ src/componensts/forms/StudentForm.jsx
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
-import "../../styles/Forms.css"
+import "../../styles/Forms.css";
 
 function StudentForm() {
-    const [fname, setFname] = useState("");
-    const [lname, setLname] = useState("");
-
-    const handleSubmit = (e) => {
-       e.preventDefault()
-       console.log(`First Name: ${fname}`);
-       console.log(`Last Name: ${lname}`);
-       
+    const defaultValues = {
+    name: "",
+    age: "",
+    isActive: true,
+  
     }
+  const [formData, setFormData] = useState(defaultValues);
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    alert("User create success ")
+
+    setFormData(defaultValues)
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <label for="fname">First name:</label>
+        <label for="name">Name:</label>
         <br />
-        <input type="text" id="fname" name="fname" onChange={(e) => setFname(e.target.value)} />
+        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
         <br />
-        <label for="lname">Last name:</label>
+        <label for="age">Age:</label>
         <br />
-        <input type="text" id="lname" name="lname" onChange={(e) => setLname(e.target.value)} />
-          <br /> <br />
+        <input type="number" id="age" name="age" value={formData.age} onChange={handleChange} />
+        <br /> <br />
         <input type="submit" />
       </form>
     </>
