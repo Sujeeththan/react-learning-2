48a7d8a96fa79a9b61991283a78ad3d3b9d27563
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-22 15:47:49 +0530

    form submit

---
+++ src/componensts/forms/StudentForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "../../styles/Forms.css"
+
+function StudentForm() {
+    const [fname, setFname] = useState("");
+    const [lname, setLname] = useState("");
+
+    const handleSubmit = (e) => {
+       e.preventDefault()
+       console.log(`First Name: ${fname}`);
+       console.log(`Last Name: ${lname}`);
+       
+    }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label for="fname">First name:</label>
+        <br />
+        <input type="text" id="fname" name="fname" onChange={(e) => setFname(e.target.value)} />
+        <br />
+        <label for="lname">Last name:</label>
+        <br />
+        <input type="text" id="lname" name="lname" onChange={(e) => setLname(e.target.value)} />
+          <br /> <br />
+        <input type="submit" />
+      </form>
+    </>
+  );
+}
+
+export default StudentForm;
