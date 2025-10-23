85d9be45b9debed1ed62543346e589329ca5f2fe
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-23 15:39:43 +0530

    get user in browser users.map((user)=> { }) add

--- src/pages/Fetch.jsx
+++ src/pages/Fetch.jsx
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from "react";
 
 function Fetch() {
-  const[users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
   const fetchUsers = async () => {
-  const response = await  fetch("https://jsonplaceholder.typicode.com/users")
-  const userData = await response.json()
-  console.log(userData);
-  
-  setUsers(users);
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const userData = await response.json();
+    console.log(userData);
+    setUsers(userData);
   };
 
-
-  return <div>Fetch</div>;
+  return (
+    <>
+      {users.map((user) => (
+       <>
+        <h1>{user.name}</h1>
+        <h2>{user.email}</h2>
+        <h3>{user.phone}</h3>
+       </>
+      ))}
+      
+    </>
+  );
 }
 
 export default Fetch;
