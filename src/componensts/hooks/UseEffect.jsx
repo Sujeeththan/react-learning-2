cb4c5c1cb5d51201a6da3722df694ca0a4d84e13
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-23 14:18:11 +0530

    Use Effect add

--- src/componensts/hooks/UseEffect.jsx
+++ src/componensts/hooks/UseEffect.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function UseEffect() {
+ const [age, setAge] = useState(27);
+ const [size, setSize] = useState(20);
+
+
+ useEffect(() => {
+  console.info("word is logging");
+ },[age]) 
+
+  function increase() {
+    setAge(age + 1);
+  }
+
+   function increase() {
+    setSize(size + 2);
+  }
+  
   return (
-    <div>UseEffect</div>
-  )
+    <>
+    <div>
+      <h2>Age: {age}</h2>
+      <button onClick={increase}>
+        Increase by 1
+      </button>
+      <br />
+      <input type="text" name="" id="" value={age}/>
+
+  
+      <h2>Size: {size}</h2>
+      <button onClick={increase}>
+        Increase by 2
+      </button>
+      <br />
+      <input type="text" name="" id="" value={size}/>
+    </div>
+    </>
+  );
 }
 
-export default UseEffect
+
+
+export default UseEffect;
