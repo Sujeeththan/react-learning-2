16abba7961a56d628eb6352d600a56225a00cef9
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-27 19:22:14 +0530

    postData function create

--- src/componensts/axios/PostData.jsx
+++ src/componensts/axios/PostData.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "../../styles/Forms.css";
+
 
 function PostData() {
+   const defaultValues = {
+      name: "",
+      age: "",
+      isActive: true,
+    
+      }
+    const [formData, setFormData] = useState(defaultValues);
+  
+    const handleChange = (e) => {
+      const { name, value, type, checked } = e.target;
+      setFormData((prev) => ({
+        ...prev,
+        [name]: type === "checkbox" ? checked : value,
+      }));
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(formData);
+      alert("User create success ")
+  
+      setFormData(defaultValues)
+    };
+
   return (
-    <div>PostData</div>
-  )
+     <>
+      <form onSubmit={handleSubmit}>
+        <label for="name">Name:</label>
+        <br />
+        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+        <br />
+        <label for="age">Age:</label>
+        <br />
+        <input type="number" id="age" name="age" value={formData.age} onChange={handleChange} />
+        <br /> <br />
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
 
-export default PostData
+export default PostData;
