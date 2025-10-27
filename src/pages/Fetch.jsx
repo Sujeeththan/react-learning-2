e910f1586893b7eb8e5e14410d5fec37c630e190
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-27 12:14:48 +0530

    add <ApiCall />

--- src/pages/Fetch.jsx
+++ src/pages/Fetch.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import JsonPlaceholder from '../componensts/fetch/JsonPlaceholder'
 import LocalApi from '../componensts/fetch/LocalApi'
+import ApiCall from '../componensts/fetch/ApiCall'
 
 function Fetch() {
   return (
    <>
    {/* <JsonPlaceholder /> */}
-   <LocalApi />
+   {/* <LocalApi /> */}
+   <ApiCall />
    </>
   )
 }
