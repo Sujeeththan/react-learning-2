b346782e821164d7bad5a0311cd59c1cb77976cf
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-27 11:57:00 +0530

    condition add

--- src/componensts/props/ConditionalRendering.jsx
+++ src/componensts/props/ConditionalRendering.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-function ConditionalRendering() {
+function ConditionalRendering({isActive = true,status}) {
   return (
-    <div>ConditionalRendering</div>
+    <div className='content-container'>
+      <h1>ConditionalRendering</h1>
+      {isActive &&  <p style={style.active}>Active</p> }
+      {status == "paid" && <p>He is Paid</p>}
+       
+      </div>
+    
   )
+}
+
+const style = {
+  active:{
+    backgroundColor:"green",
+    display:"inline",
+    borderRadius:"10px",
+    padding:"5px 10px"
+  }
 }
 
 export default ConditionalRendering
