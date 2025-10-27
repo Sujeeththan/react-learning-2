b0aca67f2ad2748827aafc4903f9874f4259c812
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-27 15:03:06 +0530

    fetchData add

---
+++ src/pages/Axios.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FetchData from '../componensts/axios/FetchData'
+
+function Axios() {
+  return (
+    <div>
+      <FetchData />
+    </div>
+  )
+}
+
+export default Axios
