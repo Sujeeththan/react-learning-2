f2253f1269659d442c19c6d24d7a0a67878062dd
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-27 19:24:18 +0530

    add <PostData />

--- src/pages/Axios.jsx
+++ src/pages/Axios.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import FetchData from '../componensts/axios/FetchData'
+import PostData from '../componensts/axios/PostData'
 
 function Axios() {
   return (
     <div>
+      <PostData />
       <FetchData />
+      
     </div>
   )
 }
