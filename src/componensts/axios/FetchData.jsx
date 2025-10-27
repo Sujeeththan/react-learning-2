d2f0a9b64a6f0000a1a019d752b1a91ef07bc068
sujeeththan <sujeethsujeeth487@gmail.com>
2025-10-27 16:25:20 +0530

    new Date toLocalString add

---
+++ src/componensts/axios/FetchData.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function FetchData() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get(
+        "https://express-mongo-connection-sigma.vercel.app/api/users"
+      );
+      const userData = res.data.users
+      console.log(userData);
+      setUsers(userData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="content-container">
+        <h1>FetchData</h1>
+        <div className="content-body">Api call using axios</div>
+      </div>
+
+      <table border={1} cellPadding={5} cellSpacing={0} align="center">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>isActive</th>
+            <th>CreatedAt</th>
+            <th>UpdatedAt</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.isActive ? "Yes" : "No"}</td>
+              <td>{new Date(user.createdAt).toLocaleString("en-US" , {
+                day: "numeric",
+                month:"2-digit",
+                year:"numeric",
+              })}</td>
+              <td>{user.updatedAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default FetchData;
